54f30d2cd176687db458bbbf683e116cd01c6c43
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-07-13 11:59:28 +0530

    Create deployed.sh

---
+++ .github/workflows/deployed.sh
@@ -0,0 +1,3 @@
+curl -X POST -H "Accept: application/vnd.github.v3+json" -u rkrupesh44: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 \
+https://api.github.com/repos/rkrupesh44/GitHub_Action/actions/workflows/first_script.yml/dispatches \
+-d '{"ref":"dev_branch"}'
