34ef550c032c4f460cdbefb9db9765dc49c9d7cf
rkrupesh44 <88672591+rkrupesh44@users.noreply.github.com>
2022-07-13 12:39:42 +0530

    Update deployment.sh

--- first_folder/.github/workflows/deployment.sh
+++ first_folder/.github/workflows/deployment.sh
@@ -1,3 +1,3 @@
-curl -X POST -H "Accept: application/vnd.github.v3+json" -u rkrupesh44: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 \
+curl -X POST -H "Accept: application/vnd.github.v3+json" -u rkrupesh44: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 \
 https://api.github.com/repos/rkrupesh44/GitHub_Action/first_folder/actions/workflows/script.yml/dispatches \
 -d '{"ref":"dev_branch"}'
